bd502e2e4a46bf8b43ef24593fbb7cb752e440bc
marciomelo@gmail.com <marciomelo@gmail.com>
2017-11-25 16:21:47 -0300

    create the model generator for prediction machine learning

---
+++ src/main/java/com/dragons/hungrydragon/precog/ModelGenerator.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dragons.hungrydragon.precog;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils;
+
+/**
+ * Model Generator for Predicting Foods by Day of Week
+ *
+ * @author Marcio
+ */
+public class ModelGenerator {
+
+    public Instances loadDataset(String path) {
+        Instances dataset = null;
+        try {
+            dataset = ConverterUtils.DataSource.read(path);
+            if (dataset.classIndex() == -1) {
+                dataset.setClassIndex(dataset.numAttributes() - 1);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return dataset;
+    }
+
+    public Classifier buildClassifier(Instances traindataset) {
+        MultilayerPerceptron m = new MultilayerPerceptron();
+
+        try {
+            m.buildClassifier(traindataset);
+
+        } catch (Exception ex) {
+            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return m;
+    }
+
+    @SuppressWarnings("null")
+    public String evaluateModel(Classifier model, Instances traindataset, Instances testdataset) {
+        Evaluation eval = null;
+        try {
+            // Evaluate classifier with test dataset
+            eval = new Evaluation(traindataset);
+            eval.evaluateModel(model, testdataset);
+        } catch (Exception ex) {
+            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return eval.toSummaryString("", true);
+    }
+
+    public void saveModel(Classifier model, String modelpath) {
+        try {
+            SerializationHelper.write(modelpath, model);
+        } catch (Exception ex) {
+            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/test/java/com/dragons/hungrydragon/precog/ModelGeneratorTests.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dragons.hungrydragon.precog;
+
+import java.io.File;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Marcio
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelGeneratorTests extends ModelGenerator {
+
+    @Test
+    public void loadDatasetWithInvalidPath() {
+        assertNull(super.loadDataset("/invalid-path/file.arff"));
+    }
+
+    @Test
+    public void loadDatasetWithValidPath() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("food.arff").getFile());
+        assertNotNull(file.getAbsolutePath());
+    }
+}
