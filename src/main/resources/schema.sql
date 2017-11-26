6b34c07a3ada47bbb24a4bb3c853f53ef1b88f14
sfahadahmed <sfahadahmed@gmail.com>
2017-11-26 14:44:36 +0500

    - Adding updated schema script - removing old schema script from "database" sub-directory

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL);
+
+DROP TABLE IF EXISTS restaurants;
+CREATE TABLE IF NOT EXISTS restaurants(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    latitude VARCHAR(255),
+    longitude VARCHAR(255));
+
+DROP TABLE IF EXISTS menu_items;
+CREATE TABLE IF NOT EXISTS menu_items(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    price DECIMAL(3,2) NOT NULL,
+    restaurant_id INT,
+    FOREIGN KEY(restaurant_id) REFERENCES restaurants(id));
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE IF NOT EXISTS orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    creation_date TIMESTAMP NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    user_id INT,
+    restaurant_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(restaurant_id) REFERENCES restaurants(id));
+
+DROP TABLE IF EXISTS orders_menu_items;
+CREATE TABLE IF NOT EXISTS orders_menu_items(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    quantity INT DEFAULT 1,
+    order_id INT,
+    menu_item_id INT,
+    FOREIGN KEY(order_id) REFERENCES orders(id),
+    FOREIGN KEY(menu_item_id) REFERENCES menu_items(id));
+
+DROP TABLE IF EXISTS food_by_week;
+CREATE TABLE IF NOT EXISTS food_by_week(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    day_of_week VARCHAR (255) NOT NULL);
